16742e046922a170bad10c44bf8a3334c9e7a82f
Andrea <vescovini.andrea@gmail.com>
2017-07-30 23:24:18 +0200

    Added stiffness matrix

--- linsys.m
+++ linsys.m
@@ -1,4 +1,4 @@
-function [A, b] = linsys( input_args )
+function [A, b] = linsys(K, Np, Jdet, Jcof)
 %linsys function that computes the matrix A and the vector b of the linear
 %system that solves the problem
 
@@ -6,15 +6,19 @@ A = zeros(K*Np); %poi da mettere sparsi
 I = zeros(K*Np);
 S = zeros(K*Np);
 b = zeros(K*Np,1);
-bi = zeros(K*Np, 1);
+bi = zeros(K*Np,1);
 bs = zeros(K*Np,1);
 
+[nod2, wei2, nod3, wei3] = quadrature;
+[dphi] = basis_grad(nod3);
 
 for ie = 1:K %loop on the elements
     
     for i = (ie-1)*Np+1:ie*Np %loop on the basis functions
-        for j = i:ie*Np
-            A(i,j) = A(i,j) + (..)/Jdet(k);
+        for j = (ie-1)*Np+1:ie*Np
+            for q = 1:5 %loop on quadrature points
+                A(i,j) = A(i,j) + (wei3(q)/abs(Jdet(ie)))*(dphi(:,q,j-(ie-1)*Np)'*Jcof(:,:,ie)'*Jcof(:,:,ie)*dphi(:,q,i-(ie-1)*Np));
+            end
         end
     end
         
--- main3D.m
+++ main3D.m
@@ -7,6 +7,7 @@ uex = @(x,y,z) exp(x.*y.*z);
 f = @(x,y,z) -exp(x.*y.*z).*((x.*y).^2+(x.*z).^2+(y.*z).^2);
 gd = uex;
 N = 1; % degree
+Np = (N+1)*(N+2)*(N+3)/6; %number of points for every element
 Nfaces = 4; % number of faces for every element
 
 % generate the mesh
@@ -26,10 +27,10 @@ z = 0.5*(-(1+r+s+t)*VZ(va)+(1+r)*VZ(vb)+(1+s)*VZ(vc)+(1+t)*VZ(vd));
 %%%%%% BISOGNA CONTROLLARE CHE SE N>1 A ME INTERESSANO SOLO I VERTICI,
 %%%%%% DIPENDE DA COME CREO PRIMA r s t, CHE TRAMITE EToV MAPPANO x y z,
 %%%%%% POTREI PER ESEMPIO METTERE I VERTICI SUBITO NEI PRIMI NODI.
-[Jdet, Jcof] = jacobians(x,y,z,r,s,t); % del determinante dovrò poi prenderne il valore assoluto
+[Jdet, Jcof] = jacobians(x,y,z,r,s,t); % del determinante dovro' poi prenderne il valore assoluto
 
 % assemble the linear system
-%[A, b] = linsys(femreg);
+[A, b] = linsys(K, Np, Jdet, Jcof);
 
 % solve the linear system
 %u = A\b;
